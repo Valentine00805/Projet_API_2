f20c494a02efcfa7c44f7f5f669beb27d2bd1e18
S Matallah <sanaamatallah112@gmail.com>
2026-03-31 10:58:34 +0200

    Ajout du select medicaments et ajout de commentaire dans controleur

---
+++ controleur/controleur.php
@@ -0,0 +1,60 @@
+<?php
+
+
+// function addEtu()
+// {
+// $nom = $_POST["nom"] ;
+// $prenom = $_POST["prenom"] ;
+// $bac = $_POST["bac"];
+// insertEtu($nom, $prenom, $bac) ; //modèle
+// }
+
+// function updEtu()
+// {
+// 	$etu=$_POST["id"];
+// $nom = $_POST["nom"] ;
+// $prenom = $_POST["prenom"] ;
+// $bac = $_POST["bac"]);
+// updateEtu($etu, $nom, $prenom, $bac) ; //modèle
+
+// }
+
+// function delEtu()
+// {
+// 	$etu=$_POST["id"];
+// 	deleteEtu($etu) ; //modèle
+// }
+
+
+// function getAllEtu()
+// {
+//  	$etudiants=selectEtu() ; //modèle
+// include ("vues/vue.php");
+
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- modele/modele.php
+++ modele/modele.php
@@ -1,24 +1,9 @@
 <?php
 //POST pour medicament et activité
 //On commencera avec MEDICAMENT
-function insertMed($comp, $nom, $eff_therap, $eff_sec, $autre, $interd, $recom){
-    $url = 'http://127.0.0.1/API/getMeds.php';
-    $data = array('composition' => $comp, 'nom' => $nom, 'effets_therapeutiques' => $eff_therap, 'effets_secondaires' => $eff_sec, 'interaction_avec_autre_medicament' => $autre, 'interdiction' => $interd, 'recommendation' => $recom);
-
-    $options = array(
-        'http' => array(
-            'header' => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method' => 'POST',
-            'content' => http_build_query($data)
-        )
-    );
-    $context = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-    Return $result;
-}
 
 function selectMed(){
-    $url = 'http://127.0.0.1/API/getMeds.php';
+    $url = 'http://127.0.0.1/PROJET_API_2/getMeds.php';
     $options = array(
         'http' => array(
             'header' => "Content-type: application/x-www-form-urlencoded\r\n",
