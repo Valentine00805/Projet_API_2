0d304c1dcf16ea8e4d6dff1b66a1542f012bfa9a
adrienbernady65-eng <adrienbernady65@gmail.com>
2026-04-02 09:36:15 +0200

    Changement du controleur + ajout du menu globale

--- controleur/controleur.php
+++ controleur/controleur.php
@@ -1,60 +1,23 @@
 <?php
-
-
-// function addEtu()
-// {
-// $nom = $_POST["nom"] ;
-// $prenom = $_POST["prenom"] ;
-// $bac = $_POST["bac"];
-// insertEtu($nom, $prenom, $bac) ; //modèle
-// }
-
-// function updEtu()
-// {
-// 	$etu=$_POST["id"];
-// $nom = $_POST["nom"] ;
-// $prenom = $_POST["prenom"] ;
-// $bac = $_POST["bac"]);
-// updateEtu($etu, $nom, $prenom, $bac) ; //modèle
-
-// }
-
-// function delEtu()
-// {
-// 	$etu=$_POST["id"];
-// 	deleteEtu($etu) ; //modèle
-// }
-
-
-// function getAllEtu()
-// {
-//  	$etudiants=selectEtu() ; //modèle
-// include ("vues/vue.php");
-
-
-// }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function ajoutInscrit()
+{
+    $nom = $_POST["nom"];
+    $prenom = $_POST["prenom"];
+    $email = $_POST["email"];
+    $date_naissance = $_POST["date_naissance"];
+    $adresse = $_POST["adresse"];
+    $telephone = $_POST["telephone"];
+    $ida = $_POST["ida"];
+    insertEtu($nom, $prenom, $email, $date_naissance, $adresse, $telephone, $ida) ; //modèle 
+}
+function getAllMed()
+{
+    $meds = selectMed() ; //modèle 
+    include("vue/medicaments.php") ; //vue
+}
+function getActi()
+{
+    $activite=selectActivite(); //modèle
+    include ("vue/activites.php");
+}
 ?>
